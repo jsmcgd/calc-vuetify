b4291b899326b7aeb986f16703b24c9a44df5f73
James Devon <jamesdevon@protonmail.com>
2021-05-09 17:47:37 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -11,24 +11,24 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js')
 
 importScripts(
-  "/calc-vuetify/precache-manifest.2cc28616729c70480680db4baf0e69b4.js"
-);
+  '/calc-vuetify/precache-manifest.2cc28616729c70480680db4baf0e69b4.js'
+)
 
-workbox.core.setCacheNameDetails({prefix: "calc-vuetify"});
+workbox.core.setCacheNameDetails({ prefix: 'calc-vuetify' })
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
+    self.skipWaiting()
   }
-});
+})
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
  * requests for URLs in the manifest.
  * See https://goo.gl/S9QRab
  */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+self.__precacheManifest = [].concat(self.__precacheManifest || [])
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
