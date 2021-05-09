1deb8f58927a9dc23b1575f502a12ebef6257f67
James Devon <jamesdevon@protonmail.com>
2021-05-09 17:24:14 +0100

    Updates

---
+++ precache-manifest.2cc28616729c70480680db4baf0e69b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "548b1e19894a314f19e3",
+    "url": "/calc-vuetify/css/app.423747a8.css"
+  },
+  {
+    "revision": "196cbb9adb099723b115",
+    "url": "/calc-vuetify/css/chunk-vendors.2d7cc23b.css"
+  },
+  {
+    "revision": "ba5df620bfc28ab0d26458a44b269cc2",
+    "url": "/calc-vuetify/index.html"
+  },
+  {
+    "revision": "548b1e19894a314f19e3",
+    "url": "/calc-vuetify/js/app.8a6cd80a.js"
+  },
+  {
+    "revision": "196cbb9adb099723b115",
+    "url": "/calc-vuetify/js/chunk-vendors.dea297fa.js"
+  },
+  {
+    "revision": "75bf0e5f20e5ad75c6a832997ce4e812",
+    "url": "/calc-vuetify/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/calc-vuetify/robots.txt"
+  }
+]);
